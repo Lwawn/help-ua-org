7082b7634d5c1078d50dbaf3d675125849062ced
yevgeniy.kit <lawn1995@gmail.com>
2022-03-23 21:58:56 +0200

    image styling

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,24 +1,29 @@
-import * as React from "react";
-import { StaticImage } from "gatsby-plugin-image";
+import * as React from "react"
+import { StaticImage } from "gatsby-plugin-image"
 import "./home.css"
 
 export const Home = () => (
-    <div className="home-block">
-       <div className="home-left">
-           <p className="text-blue">Stop War!</p>
-           <h1>Help Ukraine</h1>
-           <p className="home-description">On the morning of February 24, Russia launched a large-scale invasion of Ukraine along the entire length of the common border and from the territory of Belarus. Ukraine is fiercely resisting.</p>
-           <div className="btn-donate">Donate</div>
-           <p className="home-text">You can <span className="home-text-blue">stop russian agression!</span></p>
-       </div>
-
-       <div className="home-right">
-           <StaticImage
-               src="../../images/kyiv-freedom.png"
-               width={710}
-               height={836}
-               alt="Ukraine Free Image"
-           />
-       </div>
+  <div className="home-block">
+    <div className="home-left">
+      <p className="text-blue">Stop War!</p>
+      <h1>Help Ukraine</h1>
+      <p className="home-description">
+        On the morning of February 24, Russia launched a large-scale invasion of
+        Ukraine along the entire length of the common border and from the
+        territory of Belarus. Ukraine is fiercely resisting.
+      </p>
+      <div className="btn-donate">Donate</div>
+      <p className="home-text">
+        You can <span className="home-text-blue">stop russian agression!</span>
+      </p>
     </div>
+
+    <div className="home-right">
+      <StaticImage
+        src="../../images/kyiv-freedom.png"
+        alt="Ukraine Free Image"
+        style={{ width: "100%", height: "100vh" }}
+      />
+    </div>
+  </div>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
 import * as React from "react"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
-import { Home } from "../components/home/home";
-import {Donate} from "../components/donate/donate";
+import { Home } from "../components/home/home"
+import { Donate } from "../components/donate/donate"
 
 const IndexPage = () => (
   <Layout>
