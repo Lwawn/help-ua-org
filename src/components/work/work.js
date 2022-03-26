686239fe86155754cd104ca4e933f08eb1d57e50
vdiadychko <vitalii.diadychko@altran.com>
2022-03-26 18:14:11 +0200

    added description and work block

---
+++ src/components/work/work.js
@@ -0,0 +1,26 @@
+import * as React from "react";
+import { StaticImage } from "gatsby-plugin-image";
+import "./work.css"
+
+export const Work = () => (
+    <div className="work-block">
+        <div className="work-left">
+            <div className="work-title">What we are doing?</div>
+            <div className="white-line" />
+            <div className="work-description">
+                We created this community as well as fundraising
+                page to help victims from russian aggression with
+                delivering urgent medical supplies and humanitarian aid.
+            </div>
+        </div>
+
+        <div className="work-right">
+            <StaticImage
+                src="../../images/war.png"
+                width={690}
+                height={621}
+                alt="War Image"
+            />
+        </div>
+    </div>
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,14 +4,19 @@ import Seo from "../components/seo"
 import { Home } from "../components/home/home"
 import { Donate } from "../components/donate/donate"
 import { Contact } from "../components/contact/contact";
+import { Work } from "../components/work/work";
+import { Description } from "../components/description/description";
 
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
 
     <Home />
+    <Description />
+    <Work />
     <Contact />
     <Donate />
+
   </Layout>
 )
 
