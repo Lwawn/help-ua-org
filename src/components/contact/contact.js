8958c930891f04f5ebfc10beebde0521b4c71663
vdiadychko <vitalii.diadychko@altran.com>
2022-03-26 13:44:56 +0200

    small fix and added img

---
+++ src/components/contact/contact.js
@@ -0,0 +1,16 @@
+import * as React from "react";
+import {Link} from "gatsby";
+import { StaticImage } from "gatsby-plugin-image";
+import "./contact.css"
+
+export const Contact = () => (
+    <div className="contact-block">
+        <div className="contact-left">
+
+        </div>
+
+        <div className="contact-right">
+
+        </div>
+    </div>
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,13 +3,14 @@ import Layout from "../components/layout"
 import Seo from "../components/seo"
 import { Home } from "../components/home/home"
 import { Donate } from "../components/donate/donate"
+import { Contact } from "../components/contact/contact";
 
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
 
     <Home />
-
+    <Contact />
     <Donate />
   </Layout>
 )
